c353dce8eb2fcf596ee0a3484cdcd07428cd46ab
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-11-05 08:18:19 -0500

    Nueva activity para guardar nuevo libro

---
+++ app/src/main/java/com/example/roomexample/NewBookActivity.kt
@@ -0,0 +1,40 @@
+package com.example.roomexample
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import kotlinx.android.synthetic.main.activity_new.*
+
+class NewBookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+
+        bAdd.setOnClickListener {
+
+            val resultIntent = Intent()
+
+            if(TextUtils.isEmpty(etAuthorName.text) ||
+                TextUtils.isEmpty(etBookName.text)){
+                setResult(Activity.RESULT_CANCELED,resultIntent)
+            }else{
+                val author = etAuthorName.text.toString()
+                val book = etBookName.text.toString()
+
+                resultIntent.putExtra(NEW_AUTHOR,author)
+                resultIntent.putExtra(NEW_BOOK,book)
+            }
+            finish()
+        }
+    }
+
+    companion object{
+        const val NEW_AUTHOR = "new_author"
+        const val NEW_BOOK = "new_book"
+    }
+
+
+}
