f5f7c29eaaaf5161ce851d7234f49c609b853b23
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-11-05 09:47:39 -0500

    Creación de recyclerView

--- app/src/main/java/com/example/roomexample/MainActivity.kt
+++ app/src/main/java/com/example/roomexample/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.roomexample
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.roomexample.adapters.BookListAdapter
 import com.example.roomexample.data.BookEntity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +22,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        val bookListAdapter : BookListAdapter = BookListAdapter(this)
+        rv_books.adapter = bookListAdapter
+        rv_books.layoutManager = LinearLayoutManager(this)
 
         fab.setOnClickListener {
             val intent = Intent(this, NewBookActivity::class.java)
---
+++ app/src/main/java/com/example/roomexample/adapters/BookListAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.roomexample.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomexample.R
+import com.example.roomexample.data.BookEntity
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class BookListAdapter(private val context: Context):
+    RecyclerView.Adapter<BookListAdapter.BookViewHolder>() {
+
+    private var bookList: List<BookEntity> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+        val itemView = LayoutInflater.from(context).inflate(R.layout.list_item,parent,false)
+        return BookViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int = bookList.size
+
+    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+        val book = bookList[position]
+        holder.setData(book.authorName,book.bookName,position)
+    }
+
+    inner class BookViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+        private var pos = 0
+        fun setData(author:String,book:String,position:Int) {
+            itemView.tvAuthor.text = author
+            itemView.tvBook.text = book
+            this.pos = position
+        }
+    }
+}
